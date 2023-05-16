9a3a62b2e0bbf9667406992665c3e7246b646129
Quentin Villotta <quentin.villotta@gmail.com>
2023-05-16 13:17:42 +0200

    init

---
+++ global.R
@@ -0,0 +1,27 @@
+#  ------------------------------------------------------------------------
+#
+# Title : App - Global
+#    By : Quentin Villotta
+#  Date : 16/05/2023
+#    
+#  ------------------------------------------------------------------------
+
+
+# Packages ----------------------------------------------------------------
+
+library(shiny)
+library(googleway)
+library(dplyr)
+library(sf)
+library(readxl)
+library(colorRamps)
+
+
+
+# Funs --------------------------------------------------------------------
+
+# source("funs/summarise_etablissements.R")
+# source("funs/descriptif_application.R", encoding = "UTF-8")
+
+
+# Datas -------------------------------------------------------------------
